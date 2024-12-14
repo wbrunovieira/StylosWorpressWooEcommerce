102963c96b70e1a3a1a704846389a6c21214ab8d
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2024-12-14 09:29:05 -0300

    Removed ignored files

--- wordpress/wp-content/themes/stylos-theme/index.php
+++ wordpress/wp-content/themes/stylos-theme/index.php
@@ -1,17 +1,7 @@
 <?php get_header(); ?>
 
 <main>
-    <?php
-    if (have_posts()):
-      while (have_posts()):
-        the_post();
-        the_title('<h2>', '</h2>');
-        the_content();
-      endwhile;
-    else:
-      echo '<p>Nenhuma postagem encontrada.</p>';
-    endif;
-    ?>
+
 </main>
 
 <?php get_footer(); ?>
